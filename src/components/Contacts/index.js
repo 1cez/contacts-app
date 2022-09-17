012192833d18d4c2af7c5132f28d21bf6f361327
Serhan <srhncetin4@gmail.com>
2022-09-17 13:32:20 +0300

    filter added

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Form from "./Form";
 import List from "./List";
 
 function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Mehmet", 
+      phone_number: "123123" 
+    },
+    {
+      fullname: "Ali",
+      phone_number: "543162",
+    },
+    {
+      fullname: "Veli",
+      phone_number: "7343123",
+    },
+  ]);
+
+  useEffect(() => {}, [contacts]);
+
   return (
     <div>
-      <List />
-      <Form />
+      <List contacts={contacts} />
+      <Form addContacts={setContacts} contacts={contacts} />
     </div>
   );
 }
